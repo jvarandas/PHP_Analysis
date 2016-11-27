61adb3ca8e71d9faa365a7603b0110bba3401e8c
jvarandas <joao.varandas@tecnico.ulisboa.pt>
2016-11-27 11:45:46 +0000

    adjancy list in progress

--- src/Verify.java
+++ src/Verify.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Verify {
 
@@ -10,13 +12,38 @@ public class Verify {
 		List<List<String>> patterns = new ArrayList<List<String>>();
 		ParserPHP php_parser = new ParserPHP("sqli_01.txt");
 		List<List<String>> php_code = new ArrayList<List<String>>();
+		Map<String, String> test = new HashMap<String, String>();
 		
 
 		patterns = patterns_parser.parsePatternsList();
 		php_code = php_parser.parsePHP();
 		
 		for(List<String> s: php_code)
-			System.out.println(s);
+			for(String str: s)
+				test = varNature(patterns, str);
+		
+		System.out.println(test);
+	}
+	
+	public static Map<String,String> varNature(List<List<String>> patterns, String var){
+		
+		String nature = new String();
+		Map<String, String> var_nature = new HashMap<String, String>();
+		
+		for(List<String> l: patterns){
+			for(String s: l){
+				if(var.contains(s) && l.contains("sensitive")){
+					nature = "sensitive";
+					var_nature.put(var, nature);
+				}
+				else if(var.contains(s) && l.contains("sanitization")){
+					nature = "sanitization";
+					var_nature.put(var, nature);
+				}
+			}
+		}
+		
+		return var_nature;
 	}
 
 }
