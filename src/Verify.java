2534344299c0d56f1ee2a54100b193c5b55d394a
jvarandas <joao.varandas@tecnico.ulisboa.pt>
2016-11-27 21:03:23 +0000

    list in progress

--- src/Verify.java
+++ src/Verify.java
@@ -14,36 +14,71 @@ public class Verify {
 		List<List<String>> php_code = new ArrayList<List<String>>();
 		Map<String, String> test = new HashMap<String, String>();
 		
-
 		patterns = patterns_parser.parsePatternsList();
 		php_code = php_parser.parsePHP();
 		
-		for(List<String> s: php_code)
-			for(String str: s)
-				test = varNature(patterns, str);
+		test = varNature(patterns, php_code);
 		
 		System.out.println(test);
+		
 	}
 	
-	public static Map<String,String> varNature(List<List<String>> patterns, String var){
+	public static Map<String,String> varNature(List<List<String>> patterns, List<List<String>> code){
 		
 		String nature = new String();
+		String aux = new String();
 		Map<String, String> var_nature = new HashMap<String, String>();
 		
-		for(List<String> l: patterns){
-			for(String s: l){
-				if(var.contains(s) && l.contains("sensitive")){
-					nature = "sensitive";
-					var_nature.put(var, nature);
-				}
-				else if(var.contains(s) && l.contains("sanitization")){
-					nature = "sanitization";
-					var_nature.put(var, nature);
+		for(List<String> list: code){
+			for(String s: list){
+				
+				nature = existsIn(patterns, s);
+				if(nature != null){
+					aux = varsUsed(code, s);
+					if(aux != null){
+						var_nature.put(aux, nature);
+					}
 				}
 			}
 		}
 		
 		return var_nature;
+	}
+	
+	public static String existsIn(List<List<String>> pattern, String str_code){
+		
+		for(List<String> l: pattern){
+			for(String s: l){
+				if(str_code.contains(s) && l.contains("sensitive"))
+					return "sensitive";
+				else if(str_code.contains(s) && l.contains("sanitization"))
+					return "sanitization";
+			}
+		}
+		
+		return "neither";
+	}
+	
+	public static String varsUsed(List<List<String>> code, String var){ //REVER
+		
+		List<String> aux = new ArrayList<String>();
+		
+		if(!code.isEmpty()){
+			for(List<String> list: code){
+				if(!list.isEmpty())
+					aux.add(list.get(0));
+			}
+		}
+		
+		for(String s: aux)
+			if(var.equals(s))
+				return s;
+		
+		for(String s: aux)
+			if(var.contains(s))
+				return s;
+		
+		return null;
 	}
 
 }
