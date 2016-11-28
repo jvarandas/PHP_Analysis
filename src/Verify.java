5efb93790f498ca2a7a7e48417f4e5a7860bafb3
jvarandas <joao.varandas@tecnico.ulisboa.pt>
2016-11-28 15:28:10 +0000

    adjacency list builded

--- src/Verify.java
+++ src/Verify.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Verify {
 
@@ -12,30 +13,16 @@ public class Verify {
 		List<List<String>> patterns = new ArrayList<List<String>>();
 		ParserPHP php_parser = new ParserPHP("sqli_01.txt");
 		List<List<String>> php_code = new ArrayList<List<String>>();
-		Map<String, String> test = new HashMap<String, String>();
+		List<List<String>> dependencias = new ArrayList<List<String>>();
 		
 		patterns = patterns_parser.parsePatternsList();
 		php_code = php_parser.parsePHP();
 		
-		test = varNature(patterns, php_code);
-		
-		for(List<String> l: php_code)
-			for(String s: l)
-				System.out.println(s + " :variaveis usadas ->" + varsUsed(php_code, s));
-		
+		dependencias = buildAdjacencyList(php_code);
 		System.out.println("");
-		System.out.println(test);
-		
+		System.out.println(dependencias);
 	}
 	
-	public static List<List<String>> buildAdjacencyList(List<List<String>> patterns, List<List<String>> code){
-		
-		List<List<String>> res = new ArrayList<List<String>>();
-		List<String> nodes = new ArrayList<String>();
-		
-		nodes = generateNodes(code);
-		return res;
-	}
 	
 	public static List<String> generateNodes(List<List<String>> code){
 		
@@ -53,10 +40,39 @@ public class Verify {
 		return nodes;
 	}
 	
-	public static List<List<String>> generateDependencies(List<List<String>> code){
+	public static List<List<String>> buildAdjacencyList(List<List<String>> code){
 		
 		List<List<String>> dependencies = new ArrayList<List<String>>();
+		List<String> aux = new ArrayList<String>();
+		String vars = new String();
 		
+		if(!code.isEmpty()){
+			
+			for(List<String> l: code){
+				
+				if(l.size()>1){
+					for(int i=1; i<l.size(); i++){
+						
+						if(l.get(0).startsWith("$"))
+							aux.add(l.get(0));
+						
+						vars = varsUsed(code, l.get(i));
+						
+						if(vars != null)
+							aux.add(vars);
+						else
+							aux.add("sentence");
+					}
+				}
+				
+				if(aux != null){
+					aux = aux.stream().distinct().collect(Collectors.toList()); //REMOVER DUPLICADOS
+					dependencies.add(aux);
+				}
+				
+				aux = new ArrayList<String>();
+			}
+		}
 		
 		return dependencies;
 	}
@@ -77,7 +93,7 @@ public class Verify {
 				}
 				else{
 					if(nature.equals("sensitive") || nature.equals("sanitization"))
-						var_nature.put(aux, nature);
+						var_nature.put("sentence", nature);
 				}
 			}
 		}
